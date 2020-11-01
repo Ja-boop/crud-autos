d6bf4592adb31b451e74a7cafe11eb73574a6950
Ja-boop <agusmisiones@gmail.com>
2020-11-01 13:59:48 -0300

    Probando nunjucks

--- src/app.js
+++ src/app.js
@@ -5,10 +5,14 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port = process.env.PORT || 8080;
 
-nunjucks.configure('src/module', {
+nunjucks.configure('src/module/views', {
     autoescape: true,
     express: app
 });
+
+app.get('/', (req, res) => {
+    res.render('main.njk');
+})
 
 app.listen(port, () => {
     console.log(`Aplicacion escuchando en el puerto ${port}`);
