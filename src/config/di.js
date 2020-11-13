3a0157996787a6d0f555a948fdf9e757070a2a59
Ja-boop <agusmisiones@gmail.com>
2020-11-13 17:11:41 -0300

    Termine con con las definiciones, ahora tengo que hacer las definiciones del modulo de agencia

--- src/config/di.js
+++ src/config/di.js
@@ -1,25 +1,43 @@
+const path = require('path');
 const { default: DIContainer, object, get, factory } = require('rsdi');
 const multer = require('multer');
-const session = require('cookie-session');
+const session = require('express-session');
 
 function configureSession() {
     const ONE_WEEK_IN_SECONDS = 604800000;
 
     const sessionOptions = {
-        name: 'cookieSession',
         secret: process.env.SESSION_SECRET,
-        maxAge: ONE_WEEK_IN_SECONDS,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: ONE_WEEK_IN_SECONDS },
     };
 
     return session(sessionOptions);
 }
 
+function configureMulter() {
+    const storage = multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, process.env.CARIMAGE_UPLOAD_DIR);
+        },
+        filename(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        },
+    });
+
+    return multer({ storage });
+}
 
 function addCommonDefinitions(container) {
     container.addDefinitions({
         Session: factory(configureSession),
         Multer: factory(configureMulter),
     });
+}
+
+function addAgencyModuleDefinitions(container) {
+
 }
 
 module.export = function configureDI() {
