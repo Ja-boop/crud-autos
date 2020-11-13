078e2dc2afc878b4ede7c70b6842d613f7b5d532
Ja-boop <agusmisiones@gmail.com>
2020-11-13 16:55:40 -0300

    configurando la carpeta di

--- src/config/di.js
+++ src/config/di.js
@@ -1,1 +1,30 @@
-import DIContainer, { object, get, factory, IDIContainer } from "rsdi";
+const { default: DIContainer, object, get, factory } = require('rsdi');
+const multer = require('multer');
+const session = require('cookie-session');
+
+function configureSession() {
+    const ONE_WEEK_IN_SECONDS = 604800000;
+
+    const sessionOptions = {
+        name: 'cookieSession',
+        secret: process.env.SESSION_SECRET,
+        maxAge: ONE_WEEK_IN_SECONDS,
+    };
+
+    return session(sessionOptions);
+}
+
+
+function addCommonDefinitions(container) {
+    container.addDefinitions({
+        Session: factory(configureSession),
+        Multer: factory(configureMulter),
+    });
+}
+
+module.export = function configureDI() {
+    const container = new DIContainer();
+    addCommonDefinitions(container);
+    addAgencyModuleDefinitions(container);
+    return container;
+};
