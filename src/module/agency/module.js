fb96d76af6e72e86353f83ab69c254fd5b09bec3
Ja-boop <agusmisiones@gmail.com>
2020-11-20 16:20:08 -0300

    Update module.js

--- src/module/agency/module.js
+++ src/module/agency/module.js
@@ -1,7 +1,6 @@
 const AgencyController = require('./controller/agencyController');
 const AgencyRepository = require('./repository/sqlite/agencyRepository');
 const AgencyService = require('./service/agencyService');
-// const AgencyModel = require('./model/agencyModel');
 
 function init(app, container) {
     const controller = container.get('AgencyController');
