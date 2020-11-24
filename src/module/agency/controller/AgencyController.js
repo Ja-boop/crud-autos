6e903f8f55492ddb80a1e9a2c8cb01d1e06b6529
Ja-boop <agusmisiones@gmail.com>
2020-11-24 17:51:27 -0300

    cambios en home.njk, por el tema de los path.

--- src/module/agency/controller/AgencyController.js
+++ src/module/agency/controller/AgencyController.js
@@ -3,5 +3,38 @@ const CarIDNotDefinedEroor = require('../repository/error/carIDNotDefinedError')
 const AbstractController = require('../controller/abstractController');
 
 module.exports = class AgencyController extends AbstractController {
-    
-}
+    /**
+     * @param {import('../service/agencyService')} agencyService
+     */
+    constructor(uploadMiddleware, agencyService) {
+        super();
+        this.ROUTE_BASE = '/agency';
+        this.uploadMiddleware = uploadMiddleware;
+        this.agencyService = agencyService;
+    }
+
+    /**
+     * @param {import('express').Application} app
+     */
+    configureRoutes(app) {
+        const ROUTE = this.ROUTE_BASE;
+        
+        app.get(`${ROUTE}`, this.index.bind(this));
+        // app.get(`${ROUTE}/create`, this.create.bind(this));
+        // app.get(`${ROUTE}/view/:id`, this.view.bind(this));
+        // app.post(`${ROUTE}/save`, this.uploadMiddleware.single('image_url'), this.save.bind(this));
+        // app.get(`${ROUTE}/delete/:id`, this.delete.bind(this));
+    }
+
+    /**
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async index(req, res) {
+        const cars = await this.agencyService.getAll();
+        const { errors, messages } = req.session;
+        res.render('views/home.njk', { data: { cars }, messages, errors, logo: "public/logo/logo-luzny.png" });
+        req.session.errors = [];
+        req.session.messages = [];
+    }
+};
